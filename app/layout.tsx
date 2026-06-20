5bad777c7e1312f4d4f8ee1ed2df3766c7115194
Ridleyyy <willridleysmith@icloud.com>
2026-06-21 00:00:18 +1000

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
           <Nav />
           {children}
+          <SpeedInsights />
         </body>
     </html>
   );
